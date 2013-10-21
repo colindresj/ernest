2923cbc6800f47643f43b110ca37f1e09d5c2ba0
JC <jorge.e.colindres@gmail.com>
2013-10-21 17:03:57 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EarnestApp::Application.config.secret_token = 'd6766f24e9f4e021a3da264e9b4f64f22a332fc2de61122a0ee530bb3918afdb3d75b2cec69476429eb9c58c254e4490b9177ba219714171f905667b7188d15f'
