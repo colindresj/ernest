c72ec696ecb2cb38a258aeb3a34edbda97e7f223
JC <jorge.e.colindres@gmail.com>
2013-12-01 21:23:43 -0500

    refactored documents_controller to use :get_user on before_filter in order to scope the document search only to a particular user

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,11 +1,10 @@
 class DocumentsController < ApplicationController
-
   before_filter :authorize
   before_filter :correct_user
+  before_filter :get_user
 
   def index
     unless params[:q]
-      @user = User.find params[:user_id]
       redirect_to user_path(@user)
     else
       @documents = Document.search params[:q]
@@ -13,34 +12,29 @@ class DocumentsController < ApplicationController
   end
 
   def new
-    @user = User.find params[:user_id]
     @document = Document.new
   end
 
   def create
-    user = User.find params[:user_id]
-    document = user.documents.create params[:document]
-
+    document = @user.documents.create params[:document]
     document.create_dbox_file(dropbox_client)
 
-    redirect_to edit_user_document_path(user, document), :notice => "#{document.title} saved."
+    redirect_to edit_user_document_path(@user, document), :notice => "#{document.title} saved."
   end
 
   def show
-    @document = Document.find params[:id]
+    @document = @user.documents.find params[:id]
   end
 
   def edit
-    @user = User.find params[:user_id]
-    @document = Document.find params[:id]
+    @document = @user.documents.find params[:id]
     @editable = Editable.new
   end
 
   def update
-    user = User.find params[:user_id]
-    document = Document.find params[:id]
-
+    document = @user.documents.find params[:id]
     db_client = dropbox_client
+
     begin
       if document.title == params[:document][:title]
         db_client.upload "#{params[:document][:title]}.md", params[:document][:content]
@@ -59,13 +53,15 @@ class DocumentsController < ApplicationController
   end
 
   def destroy
-    document = Document.find params[:id]
-
+    document = @user.documents.find params[:id]
     document.delete_dbox_file(dropbox_client)
-
     document.destroy
 
     redirect_to :back, :notice => "#{document.title} deleted."
+  end
+
+  def get_user
+    @user = User.find params[:user_id]
   end
 
 end
