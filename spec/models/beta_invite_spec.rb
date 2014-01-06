4496931b060c7f573940ece49c55e1c50041571b
JC <jorge.e.colindres@gmail.com>
2014-01-06 12:58:23 -0500

    update to beta_invite_spec

--- spec/models/beta_invite_spec.rb
+++ spec/models/beta_invite_spec.rb
@@ -3,10 +3,10 @@ require "spec_helper"
 describe BetaInvite do
   before(:each) do
     @invite = create(:beta_invite)
-    @invite.grantaccess.save!
   end
 
   it "grants access" do
+    @invite.grantaccess.save!
     expect(@invite.access).to be_true
   end
 end
