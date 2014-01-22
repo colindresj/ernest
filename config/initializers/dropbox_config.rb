79c590de9fa655645066681e59016316e1138134
JC <jorge.e.colindres@gmail.com>
2014-01-21 21:30:50 -0500

    add dropbox config initializer

---
+++ config/initializers/dropbox_config.rb
@@ -0,0 +1,3 @@
+Dropbox::API::Config.app_key = ENV['DROPBOX_APPKEY']
+Dropbox::API::Config.app_secret = ENV['DROPBOX_SECRETKEY']
+Dropbox::API::Config.mode = 'sandbox' # single-directory is "sandbox" - full access is "dropbox"
